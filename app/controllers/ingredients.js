dfd85f3d456ae741dcb47c9261185d72e3ba1a2a
Daniel Romero <romerodaniel.k90@gmail.com>
2018-07-05 22:03:47 -0700

    added mobile ui sidebar controls && styles

--- app/controllers/ingredients.js
+++ app/controllers/ingredients.js
@@ -1,8 +1,34 @@
 import Controller from '@ember/controller';
+import { debounce } from '@ember/runloop';
 
 export default Controller.extend({
+  fullSidebar: false,
+  displaySidebarToggle: false,
   queryType: 'name',
+  init() {
+    this._super(...arguments);
+    setSidebarUI.bind(this)();
+
+    window.addEventListener('resize', setSidebarUI.bind(this))
+
+    function setSidebarUI() {
+      if (window.innerWidth < 960) {
+        this.set('fullSidebar', false)
+        this.set('displaySidebarToggle', true);
+      } else {
+        this.set('displaySidebarToggle', false);
+        this.set('fullSidebar', true);
+      }
+    }
+  },
+  willDestroy() {
+    window.removeEventListener('resize', () => undefined);
+  },
   actions: {
+    toggleSidebar() {
+      let previousValue = this.get('fullSidebar');
+      this.set('fullSidebar', !previousValue)
+    },
     setQuery(type) {
       this.set('queryType', type);
     },
